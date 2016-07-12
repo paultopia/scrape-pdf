70326aeb79f0a1fc293d10b37309f3bc75d8fec7
Paul Gowder <paul.gowder@gmail.com>
2016-07-12 09:50:27 -0400

    note to self

--- content.js
+++ content.js
@@ -7,3 +7,7 @@ for (i = 0, len = links.length; i < len; i++) {
 
 // works: add click handler but doesn't keep link from loading.
 // next step is to generate own http request and turn to base64.
+
+
+// NOTE: may need to use event bubbling for dynamic content
+// http://stackoverflow.com/questions/8492344/javascript-attach-an-onclick-event-to-all-links 
