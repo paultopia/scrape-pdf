df7a4d6b4344041f8d348d089cb448b84076d664
Paul Gowder <paul.gowder@gmail.com>
2016-07-12 22:05:38 -0400

    roundtrip works.

--- cmdline.py
+++ cmdline.py
@@ -1,5 +1,6 @@
 # simple commandline code to work with the base64 strings you just got:
 
+
 # first, encode an existing pdf as base64, just to make sure we know how.
 # with open("pdf.pdf", "rb") as infile:
 #     b64 = infile.read().encode("base64")
@@ -14,7 +15,7 @@ import base64
 with open("out.txt", "r") as newin:
     pdfstr = newin.read()
 
-pdfbytes = base64.b64encode(pdfstr)
+pdfbytes = base64.b64decode(pdfstr)
 
 # for some reason, every source I can find on encoding doesn't use a lib but decoding does.  okay...
 
