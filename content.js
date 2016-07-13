850b0adb6545e98f36caf8c8b909d3a5461f2a77
Paul Gowder <paul.gowder@gmail.com>
2016-07-12 22:43:25 -0400

    comment and clean the code

--- content.js
+++ content.js
@@ -1,19 +1,23 @@
+// get an array of every link on page
+
 var links = document.getElementsByTagName("a");
+
+// function to transform arraybuffer to base64 string.  Don't ask me how it works, this part is
+// entirely from stackoverflow and people who understand how encodings work (i.e., not me).
+// also, javascript is a really janky language.  Look at how much easier it is in the python
+// code in this repo for comparison... 
 
 function transform(blob) {
     return btoa(String.fromCharCode.apply(null, new Uint8Array(blob)));
 };
+
+// function to fetch a link and massage response into arraybuffer
 
 function getlink(link) {
     var x = new XMLHttpRequest();
     x.open("GET", link, true);
     x.responseType = 'arraybuffer';
     x.onload = function(e) {
-        console.log("Raw response:");
-        console.log(x.response);
-        console.log("Direct transformation:");
-        console.log(btoa(x.response));
-        console.log("Mysterious thing I got from SO:");
         console.log(transform(x.response));
         window.location.href = link;
     };
@@ -24,6 +28,9 @@ function getlink(link) {
 
     x.send(null);
 };
+
+// hang click event handlers on all the links on the page. Make ordinary loading behavior wait until the
+// extension gets at content.
 
 for (i = 0, len = links.length; i < len; i++) {
     var l = links[i]
